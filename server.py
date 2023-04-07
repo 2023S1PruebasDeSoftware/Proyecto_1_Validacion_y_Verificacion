ffb1e0d1ac8e5bcd6d7acc94cbf34b679732388c
DSaa2000 <diego.saa@sansano.usm.cl>
2023-04-07 15:14:12 -0400

    logs server

--- server.py
+++ server.py
@@ -1,18 +1,40 @@
 import socket
-
-HOST = '25.72.66.152'  # Dirección IP del servidor
+from datetime import date
+from datetime import datetime
+print("El servidor se encuentra en ejecución...")
+HOST = 'localhost'  # Dirección IP del servidor #25.72.66.152
 PORT = 12345  # Puerto en el que escuchará el servidor
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Conexión establecida con', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data)
-            respuesta = input("Ingrese respuesta: ")
-            conn.sendall(respuesta.encode())
+today = date.today()
+now = datetime.now()
+
+timeFormat = str(now.hour)+":"+str(now.minute)+":"+str(now.second)
+
+file = open("Logs_"+str(today)+".txt","w")
+logs = ""
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  
+    try:
+        s.bind((HOST, PORT))
+        s.listen()  
+        logs += timeFormat+" Info: Servidor esperando conexión.\n"     
+        print("Servidor esperando conexión...")
+        
+        conn, addr = s.accept()
+        with conn:
+            logs += timeFormat+" Info: Conexión establecida con: "+str(addr)+"\n"
+            print('Conexión establecida con:', addr)
+            while True:
+                logs += timeFormat+" Info: Mensaje recibido.\n"
+                data = conn.recv(1024)
+                if not data or data.decode() == "Bye":
+                    logs += timeFormat+" Info: Chat finalizado.\n"
+                    break
+                print(data.decode())
+                respuesta = input("Ingrese respuesta: ")
+                conn.sendall(respuesta.encode())
+                logs += timeFormat+" Info: Mensaje enviado.\n"
+
+    except Exception as e:
+        logs += timeFormat+" Warning: Se ha perdido la conexión.\n"
+file.write(logs)
+print("Programa finalizado.")
